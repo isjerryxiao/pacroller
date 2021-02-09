25737ccbc1b8063a80304a5495ec6ca62c8692e5
Jerry <isjerryxiao@outlook.com>
2021-02-09 12:17:31 +0800

    release v0.1.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="pacroller",
-    version="0.1.0",
+    version="0.1.1",
     author="Jerry Xiao",
     author_email="pacroller@mail.jerryxiao.cc",
     description="unattended upgrade for archlinux",
